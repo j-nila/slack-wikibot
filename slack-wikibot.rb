540a99f48dab259c8130ace879c615a42b129551
Ardian Haxha <ardian@brainpress.org>
2016-08-08 03:25:53 +0200

    some fixes

--- slack-wikibot.rb
+++ slack-wikibot.rb
@@ -1,3 +1,6 @@
 require 'slack-ruby-bot'
 require 'slack-wikibot/commands/wiki'
 require 'slack-wikibot/bot'
+require 'mediawiki_api'
+client2 = MediawikiApi::Client.new "http://139.59.209.199/api.php"
+client2.log_in "ardian", "ardianardian"
