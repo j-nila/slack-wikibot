132a100b95118f202b019f8090cf9ad194f463cb
Ardian Haxha <ardian@brainpress.org>
2016-08-08 02:51:50 +0200

    adding mediawiki api

--- slack-wikibot/commands/wiki.rb
+++ slack-wikibot/commands/wiki.rb
@@ -2,10 +2,13 @@ require "mediawiki_api"
 module SlackWikibot
   module Commands
     class Wiki < SlackRubyBot::Commands::Base
+      client2 = MediawikiApi::Client.new "http://139.59.209.199/api.php"
+      client2.log_in "ardian", "ardianardian"
       match(/^!wiki create (?<titlewiki>\w*)\s(?<contentwiki>.*)$/) do |client, data, match|
         # match /^wiki create (?<location>\w*)\?$/ do |client, data, match|
         response = client2.create_page match[:titlewiki], match[:contentwiki]
         pageid =  response.data["pageid"]
+        client.say(channel: data.channel, text: "Your wiki is created http://139.59.209.199/?curid=#{pageid}")
         end
       match(/^!wiki delete (?<titlewiki>\w*)\s(?<reason>.*)$/) do |client, data, match|
         client2.delete_page match[:titlewiki], match[:reason]
