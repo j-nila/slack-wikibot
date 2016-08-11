9fcef776252bdf75db58258cb4e6aa7e73dfcb4b
Ardian Haxha <ardian@brainpress.org>
2016-08-11 03:58:50 +0200

    small fix from prettify

--- slack-wikibot/commands/wiki.rb
+++ slack-wikibot/commands/wiki.rb
@@ -5,12 +5,12 @@ module SlackWikibot
       wiki_connection = MediawikiApi::Client.new "http://MediaWiki-URL/api.php"
       wiki_connection.log_in "username", "password"
       match(/^!wiki create (?<titlewiki>\w*)\s(?<contentwiki>.*)$/) do |client, data, match |
-        response = wiki_connection.create_page match[: titlewiki], match[: contentwiki]
+        response = wiki_connection.create_page match[:titlewiki], match[:contentwiki]
         pageid = response.data["pageid"]
         client.say(channel: data.channel, text: "Your wiki is created http://MediaWiki-URL?curid=#{pageid}")
       end
       match(/^!wiki delete (?<titlewiki>\w*)\s(?<reason>.*)$/) do |client, data, match |
-        wiki_connection.delete_page match[: titlewiki], match[: reason]
+        wiki_connection.delete_page match[:titlewiki], match[:reason]
         client.say(channel: data.channel, text: "Article #{match[:titlewiki]} page is deleted. Reason: #{match[:reason]}")
       end
     end
